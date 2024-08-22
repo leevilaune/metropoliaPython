4b9b1e8b9477f7bc5d66a65a613fd24b35c2f65d
Leevi Laune <leevilaune@Leevis-Air.home>
2024-08-22 15:33:49 +0300

    tehtävä 8.1

---
+++ leevilaune/metropolia/08_relaatiotietokanta/relaatiotietokanta1.py
@@ -0,0 +1,21 @@
+import mysql.connector
+
+def get_airport(icao):
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT * FROM airport WHERE ident='{icao}'")
+    return cursor.fetchall()
+
+connection = mysql.connector.connect(host="127.0.0.1",
+                                     port=3306,
+                                     database="flight_game",
+                                     username="root",
+                                     password="secretpassword",
+                                     autocommit=True)
+
+while(True):
+    icao = input("Anna ICAO-tunnus: ")
+    if(icao==""):
+        break
+    airport = get_airport(icao)[0]
+    print(f"Nimi:  {airport[3]}")
+    print(f"Kunta: {airport[10]}")
