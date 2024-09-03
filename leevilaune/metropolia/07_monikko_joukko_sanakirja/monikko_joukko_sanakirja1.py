c5481eb43b905c552861a700e2532d9d44bae276
Leevi Laune <leevilaune@Leevis-MacBook-Air.local>
2024-09-03 09:54:02 +0300

    add comment to 7.1

--- leevilaune/metropolia/07_monikko_joukko_sanakirja/monikko_joukko_sanakirja1.py
+++ leevilaune/metropolia/07_monikko_joukko_sanakirja/monikko_joukko_sanakirja1.py
@@ -2,11 +2,18 @@ def get_season(month):
     if(month>12):
         return "Invalid Month"
     seasons = ("talvi", "kevät", "kesä", "syksy")
-    #negative indexing to account for winter months
+    """
+    negative indexing to account for winter months
     #[-4,0]= talvi
     #[-3]  = kevät
     #[-2]. = kesä
     #[-1]  = syksy
+    month is divided by 3 to account for months in a season
+    4 is subtracted from that to get negative index for the month
+    12/3-4=4-4=0 is december/winter
+    8/3-4=2-4=-2 is august/summer
+    """
+
     season_num = (month//3)-4
     print(season_num)
     season = seasons[season_num]
